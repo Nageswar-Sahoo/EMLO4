5d3f51138bc66fc3de2f3e1740d83c01296778e6
Nageswar-Sahoo <70502759+Nageswar-Sahoo@users.noreply.github.com>
2024-09-28 22:36:46 +0530

    Add files via upload

---
+++ assignment_3/src/datamodules/catdog_datamodule.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+from typing import Union
+import gdown
+import zipfile
+import os
+
+import lightning as L
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torchvision.datasets.utils import download_and_extract_archive
+
+class CatDogImageDataModule(L.LightningDataModule):
+    def __init__(self, dl_path: Union[str, Path] = "data", num_workers: int = 0, batch_size: int = 8):
+        super().__init__()
+        self._dl_path = dl_path
+        self._num_workers = num_workers
+        self._batch_size = batch_size
+
+    def prepare_data(self):
+        # Google Drive file ID (from the shareable link)
+        file_id = 'your_file_id'  # Replace this with your actual file ID
+
+        # URL to download the file
+        download_url = f"https://drive.google.com/uc?id=1X96sA534xC5_Yh37jhYSKVQ0PFtJLNNn"
+
+        # Name of the output zip file (it will be downloaded in the current directory)
+        output_zip = 'cat_images.zip'
+
+        # Download the zip file
+        gdown.download(download_url, output_zip, quiet=False)
+
+        # Extract the zip file in the current directory
+        with zipfile.ZipFile(output_zip, 'r') as zip_ref:
+           zip_ref.extractall(".")  # Extract to the current directory
+
+        # Optionally, remove the downloaded zip file after extraction
+        os.remove(output_zip)
+
+        print("Cat images downloaded and extracted successfully.")
+
+    @property
+    def data_path(self):
+        return Path(self._dl_path).joinpath("cats_and_dogs_filtered")
+
+    @property
+    def normalize_transform(self):
+        return transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+    @property
+    def train_transform(self):
+        return transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            self.normalize_transform,
+        ])
+
+    @property
+    def valid_transform(self):
+        return transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            self.normalize_transform
+        ])
+
+    def create_dataset(self, root, transform):
+        return ImageFolder(root=root, transform=transform)
+
+    def __dataloader(self, train: bool):
+        """Train/validation/test loaders."""
+        if train:
+            dataset = self.create_dataset(self.data_path.joinpath("train"), self.train_transform)
+        else:
+            dataset = self.create_dataset(self.data_path.joinpath("validation"), self.valid_transform)
+        return DataLoader(dataset=dataset, batch_size=self._batch_size, num_workers=self._num_workers, shuffle=train)
+
+    def train_dataloader(self):
+        return self.__dataloader(train=True)
+
+    def val_dataloader(self):
+        return self.__dataloader(train=False)
+
+    def test_dataloader(self):
+        return self.__dataloader(train=False)  # Using validation dataset for testing
---
+++ assignment_3/src/models/catdog_classifier.py
@@ -0,0 +1,58 @@
+import lightning as L
+import timm
+import torch
+import torch.nn.functional as F
+from torch import optim
+from torchmetrics import Accuracy
+
+class CatDogClassifier(L.LightningModule):
+    def __init__(self, lr: float = 1e-3):
+        super().__init__()
+        self.lr = lr
+
+        # Load pre-trained ResNet18 model
+        self.model = timm.create_model('resnet18', pretrained=True, num_classes=2)
+
+        # Multi-class accuracy with num_classes=2
+        self.train_acc = Accuracy(task="multiclass", num_classes=2)
+        self.val_acc = Accuracy(task="multiclass", num_classes=2)
+
+        self.save_hyperparameters()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self.model(x)
+        loss = F.cross_entropy(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        accuracy = torch.sum(preds == y).item() / len(y)
+        
+        # Log the accuracy
+        self.log('test_loss', loss, prog_bar=True)
+        self.log('test_acc', accuracy, prog_bar=True)
+        
+        return {"loss": loss, "accuracy": accuracy}
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.cross_entropy(logits, y)
+        preds = F.softmax(logits, dim=1)
+        self.train_acc(preds, y)
+        self.log("train_loss", loss, prog_bar=True)
+        self.log("train_acc", self.train_acc, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.cross_entropy(logits, y)
+        preds = F.softmax(logits, dim=1)
+        self.val_acc(preds, y)
+        self.log("val_loss", loss, prog_bar=True)
+        self.log("val_acc", self.val_acc, prog_bar=True)
+
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr=self.lr)
